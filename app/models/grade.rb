bb972d81f5ed75b9f1bd9a9f7dedd92244680e30
Henrique Cardoso de Faria <elo.henrique@gmail.com>
2015-03-03 19:34:06 -0300

    Validating uniqueness of name/year in Grade.

--- app/models/grade.rb
+++ app/models/grade.rb
@@ -1,3 +1,4 @@
 class Grade < ActiveRecord::Base
   validates :name, :year, presence: true
+  validates :name, uniqueness: { scope: :year }
 end
--- spec/models/grade_spec.rb
+++ spec/models/grade_spec.rb
@@ -4,5 +4,6 @@ RSpec.describe Grade, type: :model do
   context "validations" do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:year) }
+    it { should validate_uniqueness_of(:name).scoped_to(:year) }
   end
 end
