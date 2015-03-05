bec23ec1c12338a71a6cbeb49805acd4737fb252
Henrique Cardoso de Faria <elo.henrique@gmail.com>
2015-03-05 12:43:22 -0300

    Add email format validation.

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,6 +1,6 @@
 class Person < ActiveRecord::Base
   validates :name, presence: true
-  validates :email, uniqueness: true
+  validates :email, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
   
   has_many :enrollments, inverse_of: :person
   has_many :grades, through: :enrollments
--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -8,5 +8,19 @@ RSpec.describe Person, type: :model do
     it { should have_many(:grades).through(:enrollments) }
     it { should have_many(:attendances).inverse_of(:person) }
     it { should have_many(:meetings).through(:attendances) }
+    
+    describe "valid_email" do
+      it "should accept a valid email" do
+        person = build(:person, email: "elo@gm.com")
+        person.save
+        expect(person.errors[:email]).to be_empty
+      end
+      
+      it "should not accept a invalid email" do
+        person = build(:person, email: "elo@gmcom")
+        person.save
+        expect(person.errors[:email]).to eq(["is invalid"])
+      end
+    end
   end
 end
