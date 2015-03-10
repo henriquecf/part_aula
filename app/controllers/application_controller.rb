352e481e7045072e92931a53a18a94d23dabc627
Henrique Cardoso de Faria <elo.henrique@gmail.com>
2015-03-10 23:50:49 +0000

    Add title and home route.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   
   before_action :authenticate_user!
+  
+  def home
+    if institution = Institution.first
+      redirect_to institution_people_path(institution)
+    else
+      render text: "You need to set a institution to get started"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,4 +9,6 @@ Rails.application.routes.draw do
       end
     end
   end
+  
+  get "/" => "application#home"
 end
