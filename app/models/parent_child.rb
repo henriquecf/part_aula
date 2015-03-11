5132a02fcefc0e7486d8870a3b25a0463ca772fa
Henrique Cardoso de Faria <elo.henrique@gmail.com>
2015-03-11 11:40:14 -0300

    Add presence validation for parent and child.

--- app/models/parent_child.rb
+++ app/models/parent_child.rb
@@ -1,6 +1,7 @@
 class ParentChild < ActiveRecord::Base
+  validates :child_id, :parent_id, presence: true
+  validates :parent_id, uniqueness: { scope: :child_id }
+  
   belongs_to :parent, class_name: "Person"
   belongs_to :child, class_name: "Person"
-  
-  validates :parent_id, uniqueness: { scope: :child_id }
 end
--- spec/models/parent_child_spec.rb
+++ spec/models/parent_child_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe ParentChild, type: :model do
   context "validations" do
+    it { should validate_presence_of(:parent_id) }
+    it { should validate_presence_of(:child_id) }
     it { should belong_to(:parent).class_name("Person") }
     it { should belong_to(:child).class_name("Person") }
     it { should validate_uniqueness_of(:parent_id).scoped_to(:child_id) }
