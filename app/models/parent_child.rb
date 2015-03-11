88ceda6becc88f0dd9e84326a65075fdc6202b06
Henrique Cardoso de Faria <elo.henrique@gmail.com>
2015-03-11 11:52:13 -0300

    Add validation for parent != child.

--- app/models/parent_child.rb
+++ app/models/parent_child.rb
@@ -1,7 +1,14 @@
 class ParentChild < ActiveRecord::Base
   validates :child_id, :parent_id, presence: true
   validates :parent_id, uniqueness: { scope: :child_id }
+  validate :different_child_and_parent
   
   belongs_to :parent, class_name: "Person"
   belongs_to :child, class_name: "Person"
+  
+  private
+  
+  def different_child_and_parent
+    errors.add(:child_id, "Child must be different from parent") if parent_id == child_id
+  end
 end
--- spec/models/parent_child_spec.rb
+++ spec/models/parent_child_spec.rb
@@ -7,5 +7,14 @@ RSpec.describe ParentChild, type: :model do
     it { should belong_to(:parent).class_name("Person") }
     it { should belong_to(:child).class_name("Person") }
     it { should validate_uniqueness_of(:parent_id).scoped_to(:child_id) }
+    
+    describe "different_child_and_parent" do
+      it "should not let equal parent_id and child_id" do
+        person = create(:person)
+        parent_child = build(:parent_child, parent_id: person.id, child_id: person.id)
+        parent_child.save
+        expect(parent_child.errors[:child_id]).to eq(["Child must be different from parent"])
+      end
+    end
   end
 end
