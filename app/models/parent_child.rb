0f3526ca6ef1d90f07086bb1b8f0da371c6877c6
Henrique Cardoso de Faria <elo.henrique@gmail.com>
2015-03-11 11:34:37 -0300

    Set uniqueness for parent child relation.

--- app/models/parent_child.rb
+++ app/models/parent_child.rb
@@ -1,4 +1,6 @@
 class ParentChild < ActiveRecord::Base
   belongs_to :parent, class_name: "Person"
   belongs_to :child, class_name: "Person"
+  
+  validates :parent_id, uniqueness: { scope: :child_id }
 end
--- spec/models/parent_child_spec.rb
+++ spec/models/parent_child_spec.rb
@@ -4,5 +4,6 @@ RSpec.describe ParentChild, type: :model do
   context "validations" do
     it { should belong_to(:parent).class_name("Person") }
     it { should belong_to(:child).class_name("Person") }
+    it { should validate_uniqueness_of(:parent_id).scoped_to(:child_id) }
   end
 end
