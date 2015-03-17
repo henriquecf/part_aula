78a3463c2cca2fda4ceddab140373d63a974d171
Henrique Cardoso de Faria <elo.henrique@gmail.com>
2015-03-17 16:50:16 -0300

    Change validations.

--- spec/models/enrollment_spec.rb
+++ spec/models/enrollment_spec.rb
@@ -7,5 +7,6 @@ RSpec.describe Enrollment, type: :model do
     it { should validate_presence_of(:role) }
     it { should validate_presence_of(:person_id) }
     it { should validate_presence_of(:grade_id) }
+    it { should validate_uniqueness_of(:person_id).scoped_to(:grade_id) }
   end
 end
